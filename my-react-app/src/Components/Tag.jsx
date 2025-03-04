ca2143b0d14d18cbf37443f560bc4d3ce627178e
Alex <salmon_alexandre@outlook.fr>
2025-03-04 17:23:43 +0100

    [reform] more adjustements i n scss

--- my-react-app/src/Components/Tag.jsx
+++ my-react-app/src/Components/Tag.jsx
@@ -1,15 +1,21 @@
 import '../Styles/Tag.scss';
 
 function Tags({ logement }) {
-  if (!logement || !logement.tags || logement.tags.length === 0) {
-    return <p>No Tags available</p>;
+  if (!logement || !logement.host || !logement.host.name) {
+    return <p>No Host Available</p>;
   }
+
+  const nameParts = logement.host.name.split(" "); 
 
   return (
     <div className='host'>
-            <p>{logement.host.name}</p>
-            <img src={logement.host.picture} className="avatar" alt="avatar" />
-        </div>
+      <p className="host-name">
+        {nameParts.map((part, index) => (
+          <span key={index}>{part}<br /></span> 
+        ))}
+      </p>
+      <img src={logement.host.picture} className="avatar" alt="avatar" />
+    </div>
   );
 }
 
